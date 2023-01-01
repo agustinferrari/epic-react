7835d45d494b79fca517eaf70a3aff490e175e9c
Agustin Ferrari <agustin_ferrari@hotmail.com>
2022-12-31 22:34:01 -0300

    5

--- src/final/05.js
+++ src/final/05.js
@@ -1,42 +1,36 @@
 // Styling
-// http://localhost:3000/isolated/final/05.js
+// 💯 accept a size prop to encapsulate styling
+// http://localhost:3000/isolated/final/05.extra-2.js
 
 import * as React from 'react'
 import '../box-styles.css'
 
-const smallBox = (
-  <div
-    className="box box--small"
-    style={{fontStyle: 'italic', backgroundColor: 'lightblue'}}
-  >
-    small lightblue box
-  </div>
-)
-const mediumBox = (
-  <div
-    className="box box--medium"
-    style={{fontStyle: 'italic', backgroundColor: 'pink'}}
-  >
-    medium pink box
-  </div>
-)
-const largeBox = (
-  <div
-    className="box box--large"
-    style={{fontStyle: 'italic', backgroundColor: 'orange'}}
-  >
-    large orange box
-  </div>
-)
+function Box({ style, size, className = '', ...otherProps }) {
+    const sizeClassName = size ? `box--${size}` : ''
+    return (
+        <div
+            className={`box ${className} ${sizeClassName}`}
+            style={{ fontStyle: 'italic', ...style }}
+            {...otherProps}
+        />
+    )
+}
 
 function App() {
-  return (
-    <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
-    </div>
-  )
+    return (
+        <div>
+            <Box size="small" style={{ backgroundColor: 'lightblue' }}>
+                small lightblue box
+            </Box>
+            <Box size="medium" style={{ backgroundColor: 'pink' }}>
+                medium pink box
+            </Box>
+            <Box size="large" style={{ backgroundColor: 'orange' }}>
+                large orange box
+            </Box>
+            <Box>sizeless box</Box>
+        </div>
+    )
 }
 
 export default App
